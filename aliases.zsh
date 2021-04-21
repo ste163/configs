a9e0084c2941cb05f29e66146c06ea425fcd5e9b
ste163 <62910490+ste163@users.noreply.github.com>
2021-04-21 09:27:49 -0400

    Added oh-my-zsh aliases

---
+++ aliases.zsh
@@ -0,0 +1,5 @@
+# Place in /~/.oh-my-zsh/custom
+
+alias elastic-start="sudo systemctl start elasticsearch.service"
+alias elastic-stop="sudo systemctl stop elasticsearch.service"
+alias sls-start="sls offline --httpPort 5000"
